9b96e6132ffd0eed40311cc1c99265934f835e95
Jan Hradil <janohradil@gmail.com>
2025-01-04 17:34:44 +0100

    correct app name to peterhradil

--- config/database.py
+++ config/database.py
@@ -6,7 +6,7 @@ client = MongoClient(f"mongodb+srv://{config("DB_USER")}:"
                      f"{config("DB_PASSWORD")}@{config("DB_HOST")}/"
                      f"jan?retryWrites=true&w=majority")
 
-db = client.hradil
+db = client.peterhradil
  
 collection_name = db.odpovede
 
